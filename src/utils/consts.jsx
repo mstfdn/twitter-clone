e9080503f711fcfb2a1e2adc77d901e337ebd5b1
Mustafa <mstf.fdn@outlook.com>
2025-04-01 22:28:36 +0300

    fix

--- src/utils/consts.jsx
+++ src/utils/consts.jsx
@@ -217,7 +217,6 @@ export const mainMenu = [
               fill="none"
               stroke="#e7e9ea" 
               strokeWidth="2"
-              
             >
               <path
                 d="M7.323 2h11.443l-3 5h6.648L6.586 22.83 7.847 14H2.523l4.8-12zm1.354 2l-3.2 8h4.676l-.739 5.17L17.586 9h-5.352l3-5H8.677z"
@@ -231,8 +230,6 @@ export const mainMenu = [
               height={26.25} 
               className="block"
               fill="#e7e9ea"
-              stroke="#e7e9ea" 
-              strokeWidth="2"
             >
               <path
                 d="M7.323 2h11.443l-3 5h6.648L6.586 22.83 7.847 14H2.523l4.8-12zm1.354 2l-3.2 8h4.676l-.739 5.17L17.586 9h-5.352l3-5H8.677z"
@@ -251,7 +248,7 @@ export const mainMenu = [
               width={26.25} 
               height={26.25} 
               className="block"
-              fill="#e7e9ea"
+              fill="#none"
               stroke="#e7e9ea" 
               strokeWidth="2"
             >
@@ -266,9 +263,7 @@ export const mainMenu = [
               width={26.25} 
               height={26.25} 
               className="block"
-              fill="none"
               stroke="#e7e9ea" 
-              strokeWidth="2"
             >
               <path
                 d="M5.651 19h12.698c-.337-1.8-1.023-3.21-1.945-4.19C15.318 13.65 13.838 13 12 13s-3.317.65-4.404 1.81c-.922.98-1.608 2.39-1.945 4.19zm.486-5.56C7.627 11.85 9.648 11 12 11s4.373.85 5.863 2.44c1.477 1.58 2.366 3.8 2.632 6.46l.11 1.1H3.395l.11-1.1c.266-2.66 1.155-4.88 2.632-6.46zM12 4c-1.105 0-2 .9-2 2s.895 2 2 2 2-.9 2-2-.895-2-2-2zM8 6c0-2.21 1.791-4 4-4s4 1.79 4 4-1.791 4-4 4-4-1.79-4-4z"
